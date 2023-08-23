f32803006e483a91ae534143c87e184eea6f4c7b
David Benkner <d.benkner@gmail.com>
2023-08-23 14:15:36 -0400

    Add project files.

---
+++ ConsoleLibrary/Math.cs
@@ -0,0 +1,54 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace ConsoleLibrary
+{
+    public class Math
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+        public static int Mult(int x, int y)
+        {
+            return x * y;
+        }
+        public static int Div(int x, int y)
+        {
+            try
+            {
+                return x / y;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot Divide by 0!");
+                return 0;
+            }
+        }
+        public static double Inverse(double x)
+        {
+            return 1 / x;
+        }
+        public static int Cubed(int x)
+        {
+            return x * x * x;
+        }
+        public static double Power(int x, int y)
+        {
+            double ans = x;
+            for (int i = 1; i < y - 1; i++)
+            {
+                ans *= ans;
+            }
+            return ans;
+        }
+        public static int Mod(int x, int y)
+        {
+            int ans = x / y;
+            return ans;
+        }
+    }
+}
---
+++ LibraryExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using clib = ConsoleLibrary; // can use using statement to create an alias 
+var x = 12;
+var y = 0;
+
+Console.WriteLine($"{x} + {y} = {clib.Math.Add(x,y)}");
+Console.WriteLine($"{x} - {y} = {clib.Math.Sub(x, y)}");
+Console.WriteLine($"{x} * {y} = {clib.Math.Mult(x, y)}");
+Console.WriteLine($"{x} / {y} = {clib.Math.Div(x, y)}");
+Console.WriteLine(clib.Math.Power(12, 4));
+Console.WriteLine(clib.Math.Cubed(5));
+Console.WriteLine($"{clib.Math.Inverse(12):F4}");
+Console.WriteLine(clib.Math.Mod(5, 4));
+Console.WriteLine(5 % 4);
